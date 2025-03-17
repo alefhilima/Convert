1542e93abb2f1a48bbf61567491a98b9207d3713
alefhilima <alefhilima@gmail.com>
2025-03-17 10:17:55 -0400

    Capturando os elementos, capturando o valor do input, aceitando apenas números

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+
+amount.addEventListener("input", () => {
+    
+    const hasCaractersRegex = /\D+/g
+    amount.value = amount.value.replace(hasCaractersRegex, "")
+})
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+}
